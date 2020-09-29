0f35e83fcd7a6f11a59bfcf82eba0042a2b3344a
webzahidul <webzahidul@gmail.com>
2020-09-29 11:02:01 +0600

    true-vs-false

---
+++ true-false.js
@@ -0,0 +1,25 @@
+//Falsy Values
+
+// 0
+// ""
+// undefined
+//null
+//NaN
+
+
+//Truthy
+
+//"0"
+//" "
+//"false"
+// []
+//
+
+
+const name = "0";
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
