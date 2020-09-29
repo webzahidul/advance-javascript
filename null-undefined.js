cf2746d6616be0a2d3c1bb10f520cfc95f57ef19
webzahidul <webzahidul@gmail.com>
2020-09-29 11:24:43 +0600

    null-vs-undefined declearation

---
+++ null-undefined.js
@@ -0,0 +1,21 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2) 
+    return
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const result = add (13);
+console.log(result);
+
+
+const premik = (name: "khan", phone: 4547452);
+console.log(primik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 4]
+console.log(ages[11]);
